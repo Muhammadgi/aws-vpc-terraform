1dc7cff440222a093285eb60b0023b7e08d760e8
Muhammadgi <khanhkharis1@gmail.com>
2026-05-17 17:31:38 +0000

    added subnet association table for internet connectivity.

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -37,3 +37,21 @@ resource "aws_internet_gateway" "gw" {
     Name = "ecommerce-igw"
   }
 }
+
+resource "aws_route_table" "ecommerce_rtb" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "ecommerce-public-rtb"
+  }
+}
+
+resource "aws_route_table_association" "rtb_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.ecommerce_rtb.id
+}
